d61cf409adc317476e0064e9cd40300a302e7b4c
Michael Crandall <mec192@gmail.com>
2023-02-22 04:38:26 -0600

    editing json

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import twilightImage from "../../assets/small/WhachaListening2.png";
-import flowerImage from "../../assets/small/worout4u_screenshot.png";
-import sphereImage from "../../assets/small/carbon_on_wheels.png";
+import listeningImage from "../../assets/small/WhachaListening2.png";
+import workoutImage from "../../assets/small/worout4u_screenshot.png";
+import carbonImage from "../../assets/small/carbon_on_wheels.png";
 
 function Portfolio() {
   return (
@@ -11,13 +11,63 @@ function Portfolio() {
           <h2 className="section-title secondary-border">Work</h2>
         </div>
 
+        
+
+        <div className="job">
+          <div className="job-info">
+            <div className="job-img">
+              <a href="https://github.com/capriciouscalzone/WorkoutForYou">
+                {" "}
+                <img
+                  src={workoutImage}
+                  className="my-2"
+                  style={{ width: "100%" }}
+                  alt="WorkoutForYou"
+                />
+              </a>
+            </div>
+            <div className="job-text">
+              <h4>Workout4U</h4>
+              <p>
+                A full MERN stack app with a place for users to login, track their workouts, post about their motivations, 
+                and leave comments on other users motivations.  After you logged into your profile it also spit out three random workouts 
+                for you to do for the day.  {" "}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="job">
+          <div className="job-info">
+            <div className="job-img">
+              <a href="https://github.com/capriciouscalzone/WhatchaListeningTo">
+                {" "}
+                <img
+                  src={listeningImage}
+                  className="my-2"
+                  style={{ width: "100%" }}
+                  alt="WhatchaListeningTo"
+                />
+              </a>
+            </div>
+            <div className="job-text">
+              <h4>WhatchaListeningTo</h4>
+              <p>
+                A social media type app that uses databases to track and store user data and the login auth process. 
+                 A site where you can post the songs you are listening too in that moment.  
+                 It allows for all the users to see everyones posts and comment/interact with the posts.  {" "}
+              </p>
+            </div>
+          </div>
+        </div>
+
         <div className="job">
           <div className="job-info">
             <div className="job-img">
               <a href="https://github.com/capriciouscalzone/Carbon_Footprint_Project_1">
                 {" "}
                 <img
-                  src={sphereImage}
+                  src={carbonImage}
                   className="my-2"
                   style={{ width: "100%" }}
                   alt="Carbon-on-wheels"
@@ -25,62 +75,12 @@ function Portfolio() {
               </a>
             </div>
             <div className="job-text">
-              <h4>Sorcerer's Sphere</h4>
+              <h4>Carbon on Wheels</h4>
               <p>
-                A MERN application that lets the user log in and create an
-                account. The user can receive daily affirmations. The user also
-                has the ability to ask questions to receive an answer. The user
-                also can receive a lucky number if they want. The user can
-                donate money to the website if they want to.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="job">
-          <div className="job-info">
-            <div className="job-img">
-              <a href="https://lemming97.github.io/Sweet-Pea-Flowers/">
-                {" "}
-                <img
-                  src={flowerImage}
-                  className="my-2"
-                  style={{ width: "100%" }}
-                  alt="Sweet-Pea-Flowers"
-                />
-              </a>
-            </div>
-            <div className="job-text">
-              <h4>Sweet Pea Flowers</h4>
-              <p>
-                A HTMl, CSS, and Javascript application that lets the user shop
-                for their favorite flowers.The user can sign in as a guest or as
-                a user to add items to their cart. The user can manage their
-                cart by using local storage. The user can checkout and validate
-                their credit card.{" "}
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="job">
-          <div className="job-info">
-            <div className="job-img">
-              <a href="https://lemming97.github.io/Twilight_Ristorante/">
-                {" "}
-                <img
-                  src={twilightImage}
-                  className="my-2"
-                  style={{ width: "100%" }}
-                  alt="Twilight_Ristorante"
-                />
-              </a>
-            </div>
-            <div className="job-text">
-              <h4>Twilight Ristorante</h4>
-              <p>
-                A HTML and CSS application that lets the user explore a Sample
-                Restaurant Design.{" "}
+                A simple carbon foot print calculator for road trips using two apis.  
+                One of the apis used address information to determine the trip length 
+                and the second api took that data and 
+                spit back your total carbon foot print produced.
               </p>
             </div>
           </div>
