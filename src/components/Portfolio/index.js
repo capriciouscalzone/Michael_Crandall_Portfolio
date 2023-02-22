ecccc3500c2e5708edac773585175b73a95d4eae
Michael Crandall <mec192@gmail.com>
2023-02-22 03:58:25 -0600

    editing json

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,11 +1,7 @@
 import React from "react";
-import homeBrewImage from "../../assets/small/HomeBrew.png";
-import summerPunchImage from "../../assets/small/Summer_Punch.png";
-import weatherDashImage from "../../assets/small/Weather_Dash.png";
-import noteTakerImage from "../../assets/small/NoteTaker.png";
-import twilightImage from "../../assets/small/Twilight_Ristorante.png";
-import flowerImage from "../../assets/small/Sweet-Pea-Flowers.png";
-import sphereImage from "../../assets/small/Sphere.png";
+import twilightImage from "../../assets/small/WhachaListening2.png";
+import flowerImage from "../../assets/small/worout4u_screenshot.png";
+import sphereImage from "../../assets/small/carbon_on_wheels.png";
 
 function Portfolio() {
   return (
@@ -18,13 +14,13 @@ function Portfolio() {
         <div className="job">
           <div className="job-info">
             <div className="job-img">
-              <a href="https://lemming97.github.io/Sweet-Pea-Flowers/">
+              <a href="https://github.com/capriciouscalzone/Carbon_Footprint_Project_1">
                 {" "}
                 <img
                   src={sphereImage}
                   className="my-2"
                   style={{ width: "100%" }}
-                  alt="Sweet-Pea-Flowers"
+                  alt="Carbon-on-wheels"
                 />
               </a>
             </div>
@@ -85,104 +81,6 @@ function Portfolio() {
               <p>
                 A HTML and CSS application that lets the user explore a Sample
                 Restaurant Design.{" "}
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="job">
-          <div className="job-info">
-            <div className="job-img">
-              <a href="https://nameless-mountain-77177.herokuapp.com/">
-                {" "}
-                <img
-                  src={noteTakerImage}
-                  className="my-2"
-                  style={{ width: "100%" }}
-                  alt="Note_Taker"
-                />
-              </a>
-            </div>
-            <div className="job-text">
-              <h4>Note Taker</h4>
-              <p>
-                An application that provides the user the ability to add new
-                notes, and to delete notes.{" "}
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="job">
-          <div className="job-info">
-            <div className="job-img">
-              <a href="https://lemming97.github.io/Weather_Dashboard/">
-                {" "}
-                <img
-                  src={weatherDashImage}
-                  className="my-2"
-                  style={{ width: "100%" }}
-                  alt="Weather_Dashboard"
-                />
-              </a>
-            </div>
-            <div className="job-text">
-              <h4>Weather Dashboard</h4>
-              <p>
-                A website that allows the user see the weather outlook for
-                multiple cities. The user can enter a city and see a 5-Day
-                Forecast. The user can see the temperature, humidity levels,
-                wind speed as well as the UV index.{" "}
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="job">
-          <div className="job-info">
-            <div className="job-img">
-              <a href="https://lemming97.github.io/Project_1/">
-                {" "}
-                <img
-                  src={summerPunchImage}
-                  className="my-2"
-                  style={{ width: "100%" }}
-                  alt="SummerPunch"
-                />
-              </a>
-            </div>
-            <div className="job-text">
-              <h4>Summer Punch</h4>
-              <p>
-                A website which offers users to input a drink name to have the
-                ingredients, measurements, and images display. The user can also
-                have a cocktail generated. If the user is too young, the user
-                will be redirected to another page.{" "}
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="job">
-          <div className="job-info">
-            <div className="job-img">
-              <a href="https://floating-island-17357.herokuapp.com/">
-                {" "}
-                <img
-                  src={homeBrewImage}
-                  className="my-2"
-                  style={{ width: "100%" }}
-                  alt="HomeBrew"
-                />
-              </a>
-            </div>
-            <div className="job-text">
-              <h4>HomeBrew</h4>
-              <p>
-                A plant blog that allows the user to view seasonal gallery of
-                plants after the user logs into their account. The user can also
-                add comments to the blog once they are logged in as well as
-                deleting their comments.{" "}
               </p>
             </div>
           </div>
